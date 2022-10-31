368452962b8e45a60c507f6fc1f7805f427c7b79
v-reza <mrezalf0@gmail.com>
2022-10-31 16:37:58 +0700

    feat: vercel json

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const app = express();
 const usersRouter = require("./subroutes/users");
+const pokemonsRouter = require("./subroutes/pokemons");
 
 app.use("/users", usersRouter);
+app.use("/pokemons", pokemonsRouter);
 
 module.exports = app;
