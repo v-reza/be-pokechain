7e0009b0898a0d7557c17c9482ac19d7d6930a39
WildanFajarMaulana <wildanfajar71@gmail.com>
2022-11-05 12:05:51 +0700

    complete login register logout and progress refresh token

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,6 @@
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv")
-dotenv.config()
 
 const register = async (req, res) => {
   const { username, email, password, confirmPassword } = req.body;
@@ -13,8 +11,8 @@ const register = async (req, res) => {
     const haveUser = await User.findFirst({
       where: {
         OR: [{ email: email }, { username: username }],
-      }
-    })
+      },
+    });
     if (haveUser) return res.status(400).json({ msg: "User already exists" });
 
     const salt = await bcrypt.genSalt();
@@ -26,15 +24,15 @@ const register = async (req, res) => {
         password: hashPassword,
         profile: {
           create: {
-            balance: 0
-          }
-        }
+            balance: 0,
+          },
+        },
       },
       include: {
-        profile: true
-      }
+        profile: true,
+      },
     });
-    delete users.password
+    delete users.password;
     res.status(200).json({ msg: "Register Successful", user: users });
   } catch (e) {
     res.status(500).json({ msg: e.message });
@@ -49,8 +47,8 @@ const login = async (req, res) => {
         OR: [{ email: userOrEmail }, { username: userOrEmail }],
       },
       include: {
-        profile: true
-      }
+        profile: true,
+      },
     });
     if (!user) return res.status(400).json({ msg: "User not found" });
 
@@ -58,15 +56,8 @@ const login = async (req, res) => {
     if (!match) return res.status(400).json({ msg: "Wrong Password" });
 
     const { id: userId, username, email, profile } = user;
-    const accessToken = jwt.sign(
-      { userId, email, username, profile },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: "20s",
-      }
-    );
 
-    const refreshToken = jwt.sign(
+    const refreshToken = await jwt.sign(
       { userId, email, username, profile },
       process.env.REFRESH_TOKEN_SECRET,
       {
@@ -83,24 +74,44 @@ const login = async (req, res) => {
       },
     });
 
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000,
+    const userUpdated = await User.findFirst({
+      where: {
+         id: userId,
+      },
+      include: {
+        profile: true,
+      },
     });
-    res.json({ accessToken });
+
+    const {
+      id: userIdUpdated,
+      username: usernameUpdated,
+      email: emailUpdated,
+      refresh_token: refreshTokenUpdated,
+      profile: profileUpdated,
+    } = userUpdated;
+
+    const accessToken = await jwt.sign(
+      { userIdUpdated, usernameUpdated, emailUpdated, refreshTokenUpdated, profileUpdated },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "20s",
+      }
+    );
+    res.json({ accessToken, msg: "Login Successfuls" });
   } catch (e) {
     res.status(500).json({ msg: e.message });
   }
 };
 
 const logout = async (req, res) => {
-  const { refreshToken } = req.cookies;
-  if (!refreshToken) return res.sendStatus(401);
+  const { refresh_token } = req.body;
+  if (!refresh_token) return res.sendStatus(401);
 
   try {
     const user = await User.findFirst({
       where: {
-        refresh_token: refreshToken,
+        refresh_token: refresh_token,
       },
     });
 
@@ -114,7 +125,6 @@ const logout = async (req, res) => {
         refresh_token: null,
       },
     });
-    res.clearCookie("refreshToken");
     return res.status(200).json({ msg: "Logout Successful" });
   } catch (e) {
     res.status(500).json({ msg: e.message });
@@ -123,7 +133,8 @@ const logout = async (req, res) => {
 
 const refreshToken = async (req, res) => {
   try {
-    const { refreshToken } = req.cookies;
+    const { refreshToken } = req.query;
+    console.log(refreshToken)
     if (!refreshToken) return res.sendStatus(401);
 
     const user = await User.findFirst({
@@ -131,8 +142,8 @@ const refreshToken = async (req, res) => {
         refresh_token: refreshToken,
       },
       include: {
-        profile: true
-      }
+        profile: true,
+      },
     });
     if (!user) return res.sendStatus(403);
     jwt.verify(
@@ -156,9 +167,20 @@ const refreshToken = async (req, res) => {
   }
 };
 
+const users = async (req,res) =>{
+  try{
+    const user =  await User.findMany()
+    res.status(200).json(user)
+
+  }catch(e){
+    console.log(e)
+  }
+}
+
 module.exports = {
   login,
   register,
   logout,
   refreshToken,
+  users
 };
