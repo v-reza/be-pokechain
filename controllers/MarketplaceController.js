1aa99e676f89354d8abf81efcfeba9cafd1ae027
v-reza <mrezalf0@gmail.com>
2022-11-07 18:20:23 +0700

    POK-21 change field marketplace & schema prisma

--- controllers/MarketplaceController.js
+++ controllers/MarketplaceController.js
@@ -1,33 +1,43 @@
-const { Pokemon, User, MarketPokemon } = require("../models");
+const { Pokemon, User, MarketPokemon, MarketPlace } = require("../models");
 
 const recentSales = async (req, res) => {
   try {
     const marketPokemon = await MarketPokemon.findMany({
+      take: 9,
       include: {
-        detail_market_pokemon: true,
         buyer: {
           include: {
-            user: true,
-          },
+            user: true
+          }
         },
-        seller: {
+        marketplace: {
           include: {
-            user: true,
+            seller: {
+              include: {
+                user: true
+              }
+            }
           },
-        },
+        }
       },
       orderBy: {
-        created_at: "desc",
-      },
-      take: 9,
-    });
+        created_at: "desc"
+      }
+    })
+    
 
-    marketPokemon.map((item, _) => {
-      delete item.seller.user.password;
-      delete item.seller.user.refresh_token;
+    marketPokemon.map((item) => {
       delete item.buyer?.user.password;
       delete item.buyer?.user.refresh_token;
-    });
+      delete item.marketplace.seller.user.password;
+      delete item.marketplace.seller.user.refresh_token;
+      delete item.marketplace.seller.user.email;
+      delete item.marketplace.seller.balance
+      delete item.marketplace.seller.point
+      delete item.marketplace.seller.tier
+      delete item.marketplace.seller.total_sales
+      delete item.marketplace.seller.increment_id
+    })
 
     return res.status(200).json({ results: marketPokemon });
   } catch (error) {
