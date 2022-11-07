1feb2452f51bb64634318332848e8ab6cced5736
v-reza <mrezalf0@gmail.com>
2022-11-07 19:34:09 +0700

    POK-59 all pokemon pages with pagination

--- controllers/PokemonController.js
+++ controllers/PokemonController.js
@@ -1,44 +1,54 @@
-const { Pokemon, User } = require("../models");
+const { Pokemon, User, MarketPokemon } = require("../models");
 
-const pokemonsGetController = async (req, res) => {
+const getAllPokemons = async (req, res) => {
   try {
-    
-    // const user = await User.findFirst({
-    //   where: {
-    //     username: "reza"
-    //   },
-    //   include: {
-    //     profile: {
-    //       include: {
-    //         my_pokemons: {
-    //           include: {
-    //             detail_pokemon: true
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // })
-    // const pokemon = await Pokemon.findMany({
-    //   include: {
-    //     pokemon_evolutions: true
-    //   }
-    // })
-    // const evolution = []
-    // user.profile.my_pokemons.map((item, _) => {
-    //   pokemon.filter((pk) => {
-    //     if (pk.name === item.detail_pokemon.name) {
-    //       evolution.push(pk)
-    //     }
-    //   })
-    // })
+    const { page } = req.query;
+    const allMarketPokemon = await MarketPokemon.findMany()
+    const marketPokemon = await MarketPokemon.findMany({
+      skip: page ? (page - 1) * 12 : 0,
+      take: page ? 12 : 100,
+      include: {
+        buyer: {
+          include: {
+            user: true,
+          },
+        },
+        marketplace: {
+          include: {
+            seller: {
+              include: {
+                user: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    marketPokemon.map((item) => {
+      delete item.buyer?.user.password;
+      delete item.buyer?.user.refresh_token;
+      delete item.marketplace.seller.user.password;
+      delete item.marketplace.seller.user.refresh_token;
+      delete item.marketplace.seller.user.email;
+      delete item.marketplace.seller.balance;
+      delete item.marketplace.seller.point;
+      delete item.marketplace.seller.tier;
+      delete item.marketplace.seller.total_sales;
+      delete item.marketplace.seller.increment_id;
+    });
 
-    // return res.status(200).json({ msg: "success", evolution, user });
+    return res.status(200).json({
+      hasPrevious: page > 1,
+      hasNext: marketPokemon.length === 12,
+      total: allMarketPokemon.length,
+      totalPages: Math.ceil(allMarketPokemon.length / 12),
+      results: marketPokemon,
+    });
   } catch (error) {
     return res.status(500).json({ err: error.message });
   }
 };
 
 module.exports = {
-  pokemonsGetController,
+  getAllPokemons,
 };
