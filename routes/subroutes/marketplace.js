8482fcda8ea144138f29678d5bfa042ae507cd26
v-reza <mrezalf0@gmail.com>
2022-11-09 15:28:07 +0700

    POK-22 add marketplace api recent-listings

--- routes/subroutes/marketplace.js
+++ routes/subroutes/marketplace.js
@@ -1,9 +1,10 @@
-const { recentSales, overallStats } = require("../../controllers/MarketplaceController");
+const { recentSales, overallStats, recentListings } = require("../../controllers/MarketplaceController");
 
 const router = require("express").Router();
 
 router.get("/recent-sales", recentSales);
 router.get("/overall-stats", overallStats);
+router.get("/recent-listings", recentListings)
 
 
 module.exports = router;
